b8189922836df05ed1d957b521fe53d7014cf80d
Ty <126916790+Ikigai4Ever@users.noreply.github.com>
2025-03-01 22:25:53 -0600

    Update 4-7_design.vhd

--- Homework/HW3/4-7_design.vhd
+++ Homework/HW3/4-7_design.vhd
@@ -5,15 +5,16 @@ library IEEE;
 use IEEE.STD_LOGIC_1164.all;
 
 --defining inputs and outputs
-entity grayToBinary is port(
-  w, x, y, z	: in std_logic;
-  A, B, C, D	: out std_logic);
-end XOR2;
+entity grayToBinary is 
+port( w, x, y, z	: in std_logic;
+  	  A, B, C, D	: out std_logic);
+end grayToBinary;
 
-architecture Behavioral of grayToBinary is
+architecture rtl of grayToBinary is
 begin 
     A <= w;
     B <= A xor x;
     C <= B xor y;
     D <= C xor z;
- end Behavioral;
+ end rtl;
+
