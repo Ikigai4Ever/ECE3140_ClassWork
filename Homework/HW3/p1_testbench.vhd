b8fbe610a7178ce01c8782cfaa423f5012197d86
Ty <ty.h.ahrens@gmail.com>
2025-03-02 17:59:01 -0600

    finished first iteration of p1_testbench

--- Homework/HW3/p1_testbench.vhd
+++ Homework/HW3/p1_testbench.vhd
@@ -21,39 +21,55 @@ architecture BENCH of testbench is
     	begin
             I <= "0000";
             wait for 10 ns;
+            assert (F = '0' & G = '1') report "Wrong input for " & std_logic'image(I(3)) & std_logic'image(I(2)) & std_logic'image(I(1)) & std_logic'image(I(0));
             I <= "0001";
             wait for 10 ns;
+            assert (F = '1' & G = '0') report "Wrong input for " & std_logic'image(I(3)) & std_logic'image(I(2)) & std_logic'image(I(1)) & std_logic'image(I(0));
             I <= "0010";
             wait for 10 ns;
+            assert (F = '0' & G = '1') report "Wrong input for " & std_logic'image(I(3)) & std_logic'image(I(2)) & std_logic'image(I(1)) & std_logic'image(I(0));
             I <= "0011";
             wait for 10 ns;
+            assert (F = '1' & G = '0') report "Wrong input for " & std_logic'image(I(3)) & std_logic'image(I(2)) & std_logic'image(I(1)) & std_logic'image(I(0));
 
             I <= "0100";
             wait for 10 ns;
+            assert (F = '0' & G = '1') report "Wrong input for " & std_logic'image(I(3)) & std_logic'image(I(2)) & std_logic'image(I(1)) & std_logic'image(I(0));
             I <= "0101";
             wait for 10 ns;
+            assert (F = '1' & G = '0') report "Wrong input for " & std_logic'image(I(3)) & std_logic'image(I(2)) & std_logic'image(I(1)) & std_logic'image(I(0));
             I <= "0110";
             wait for 10 ns;
+            assert (F = '0' & G = '1') report "Wrong input for " & std_logic'image(I(3)) & std_logic'image(I(2)) & std_logic'image(I(1)) & std_logic'image(I(0));
             I <= "0111";
             wait for 10 ns;
+            assert (F = '1' & G = '0') report "Wrong input for " & std_logic'image(I(3)) & std_logic'image(I(2)) & std_logic'image(I(1)) & std_logic'image(I(0));
 
             I <= "1000";
             wait for 10 ns;
+            assert (F = '0' & G = '0') report "Wrong input for " & std_logic'image(I(3)) & std_logic'image(I(2)) & std_logic'image(I(1)) & std_logic'image(I(0));
             I <= "1001";
             wait for 10 ns;
+            assert (F = '0' & G = '0') report "Wrong input for " & std_logic'image(I(3)) & std_logic'image(I(2)) & std_logic'image(I(1)) & std_logic'image(I(0));
             I <= "1010";
             wait for 10 ns;
+            assert (F = '0' & G = '0') report "Wrong input for " & std_logic'image(I(3)) & std_logic'image(I(2)) & std_logic'image(I(1)) & std_logic'image(I(0));
             I <= "1011";
             wait for 10 ns;
+            assert (F = '0' & G = '0') report "Wrong input for " & std_logic'image(I(3)) & std_logic'image(I(2)) & std_logic'image(I(1)) & std_logic'image(I(0));
 
             I <= "1100";
             wait for 10 ns;
+            assert (F = '0' & G = '0') report "Wrong input for " & std_logic'image(I(3)) & std_logic'image(I(2)) & std_logic'image(I(1)) & std_logic'image(I(0));
             I <= "1101";
             wait for 10 ns;
+            assert (F = '0' & G = '0') report "Wrong input for " & std_logic'image(I(3)) & std_logic'image(I(2)) & std_logic'image(I(1)) & std_logic'image(I(0));
             I <= "1110";
             wait for 10 ns;
+            assert (F = '1' & G = '1') report "Wrong input for " & std_logic'image(I(3)) & std_logic'image(I(2)) & std_logic'image(I(1)) & std_logic'image(I(0));
             I <= "1111";
             wait for 10 ns;
+            assert (F = '1' & G = '1') report "Wrong input for " & std_logic'image(I(3)) & std_logic'image(I(2)) & std_logic'image(I(1)) & std_logic'image(I(0));
 
             I <= "0000";
             report "Program Finished";
