9fd625e8b558b0fb01b1f61b2fc0da9ec9867168
Ty <ty.h.ahrens@gmail.com>
2025-03-02 23:38:19 -0600

    added second struct to the program

--- Homework/HW3/p8_design.vhd
+++ Homework/HW3/p8_design.vhd
@@ -34,10 +34,86 @@ architecture struct1 of hw3p8 is
         -- Simplified expression
         U1: and2 port map (In1 => not_y, In2 => z, Out1 => node1);
         U2: and2 port map (In1 => x, In2 => y, Out1 => node2);
-        U3: or2 port map (In1 => node1, In2 => node2, Out1 => O);
-    
+        U3: or2 port map (In1 => node1, In2 => node2, Out1 => O);    
 
 end struct1;
+
+architecture struct2 of hw3p8 is
+    signal node1, node2, not_y : std_logic;    
+
+    component inv_d1
+        port(In1 : IN std_logic;
+             Out1 : OUT std_logic);
+    end component;
+
+    component and2_d3
+        port (In1, In2 : IN std_logic;
+              Out1      : OUT std_logic);
+    end component;
+
+    component or2_d3
+        port(In1, In2    : IN std_logic;
+             Out1        : OUT std_logic);
+    end component or2_d3;
+
+    begin
+        -- Invert two inputs
+        U4: inv_d1 port map (In1 => y, Out1 => not_y);
+            
+        -- Simplified expression
+        U5: and2_d3 port map (In1 => not_y, In2 => z, Out1 => node1);
+        U6: and2_d3 port map (In1 => x, In2 => y, Out1 => node2);
+        U7: or2_d3 port map (In1 => node1, In2 => node2, Out1 => O);
+
+end struct2;
+
+--------------------------------
+-- Description of inv_d1 gate --
+--------------------------------
+library IEEE;
+use IEEE.STD_LOGIC_1164.all;
+entity inv_d1 is
+    port(In1 : IN std_logic;
+        Out1 : OUT std_logic);
+end inv_d1;
+
+architecture rtl of inv_d1 is 
+    begin 
+        Out1 <= not In1 after 1 ns;
+end architecture rtl; 
+
+---------------------------------
+-- Description of and2_d3 gate --
+---------------------------------
+library IEEE;
+use IEEE.STD_LOGIC_1164.all;
+entity and2_d3 is
+    port (In1, In2: IN std_logic;
+            Out1  : OUT std_logic);
+end and2_d3;
+
+architecture rtl of and2_d3 is
+    begin 
+        Out1 <= In1 and In2 after 3 ns;
+end architecture rtl;
+
+
+--------------------------------
+-- Description of or2_d3 gate --
+--------------------------------
+library IEEE;
+use IEEE.STD_LOGIC_1164.all;
+entity or2_d3 is
+    port(In1, In2    : IN std_logic;
+            Out1        : OUT std_logic);
+end or2_d3;
+
+architecture rtl of or2_d3 is
+    begin 
+        Out1 <= In1 or In2 after 3 ns;
+end architecture rtl;
+
+--------------------------------------------------------------
 
 ------------------------------
 -- Description of not gate  --
@@ -49,10 +125,10 @@ entity not1 is
           Out1  : OUT std_logic);
 end not1;
 
-architecture struct1 of not1 is 
+architecture rtl of not1 is 
     begin 
         Out1 <= not In1;
-end architecture struct1;
+end architecture rtl;
 
 ------------------------------
 -- Description of and2 gate --
@@ -64,10 +140,10 @@ entity and2 is
           Out1     : OUT std_logic);
 end and2;
 
-architecture struct1 of and2 is
+architecture rtl of and2 is
     begin 
         Out1 <= In1 and In2;
-end architecture struct1;
+end architecture rtl;
 
 ------------------------------
 -- Description of or2 gate --
@@ -79,7 +155,7 @@ entity or2 is
           Out1     : OUT std_logic);
 end or2;
 
-architecture struct1 of or2 is
+architecture rtl of or2 is
     begin 
         Out1 <= In1 or In2;
-end architecture struct1;
+end architecture rtl;
