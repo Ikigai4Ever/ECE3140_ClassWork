ebbc498265af3db9d041a7ad4bdbfe9f9fbcf96e
Ty <ty.h.ahrens@gmail.com>
2025-03-25 00:46:19 -0500

    created manula_Clock signal to choose when to use the board clokc and when to use the button to switch states

--- Homework/HW5/hw5p4.vhd
+++ Homework/HW5/hw5p4.vhd
@@ -32,62 +32,37 @@ architecture behavior of hw5p4 is
 begin
     hw5p4_CLK_inst : hw5p4_CLK PORT MAP (inclk0 => inCLK, c0 => CLK);
 	
-	--manCLK <= not KEY(0); 
+    manual_Clock <= KEY(0) when SW0 = '1' else CLK;
 
 	--use the Galois LFSRs in order to generate random numbers
-    RAND_GENERATE : process(CLK, KEY, SW0)
+    RAND_GENERATE : process(manual_Clock, KEY, SW0)
     begin 
-        if SW0 = '1' then
-            --check to see if key0 is pressed to act as the clock
-            if KEY(0) = '1' then
-                --generate random for bit0 of PRNG
-                LFSR1(0) <= LFSR1(1);
-                LFSR1(1) <= LFSR1(2);
-                LFSR1(2) <= LFSR1(3);
-                LFSR1(3) <= LFSR1(3) xor LFSR1(1); 
-            --if key1 is pressed, then set all for the bits in the LFSR
-            elsif KEY(1) = '0' then 
-                LFSR1 <= "1001";
-                LFSR2 <= "1001";
-                LFSR3 <= "1001";
-                LFSR4 <= "1001";
-            end if;
         
-        --run this when the SW0 is toggled off 
-        else
-            --if key1 is pressed, then set all for the bits in the LFSR
-            if KEY(1) = '0' then 
-                LFSR1 <= "1001";
-                LFSR2 <= "1001";
-                LFSR3 <= "1001";
-                LFSR4 <= "1001";
-            
-            --increment the LFSR every clock cycle
-            elsif rising_edge(CLK) then
-                --generate random for bit0 of PRNG
-                LFSR1(0) <= LFSR1(1);
-                LFSR1(1) <= LFSR1(2);
-                LFSR1(2) <= LFSR1(3);
-                LFSR1(3) <= LFSR1(3) xor LFSR1(2);
+        --increment the LFSR every clock cycle
+        if rising_edge(manual_Clock) then
+            --generate random for bit0 of PRNG
+            LFSR1(0) <= LFSR1(1);
+            LFSR1(1) <= LFSR1(2);
+            LFSR1(2) <= LFSR1(3);
+            LFSR1(3) <= LFSR1(3) xor LFSR1(2);
 
-                --generate random for bit1 of PRNG
-                LFSR2(0) <= LFSR2(1);
-                LFSR2(1) <= LFSR2(2);
-                LFSR2(2) <= LFSR2(3);
-                LFSR2(3) <= LFSR2(3) xor LFSR2(1);
+            --generate random for bit1 of PRNG
+            LFSR2(0) <= LFSR2(1);
+            LFSR2(1) <= LFSR2(2);
+            LFSR2(2) <= LFSR2(3);
+            LFSR2(3) <= LFSR2(3) xor LFSR2(1);
 
-                --generate random for bit2 of PRNG
-                LFSR3(0) <= LFSR3(1);
-                LFSR3(1) <= LFSR3(2);
-                LFSR3(2) <= LFSR3(3);
-                LFSR3(3) <= LFSR3(2) xor LFSR3(0);
+            --generate random for bit2 of PRNG
+            LFSR3(0) <= LFSR3(1);
+            LFSR3(1) <= LFSR3(2);
+            LFSR3(2) <= LFSR3(3);
+            LFSR3(3) <= LFSR3(2) xor LFSR3(0);
 
-                --generate random for bit3 of PRNG
-                LFSR4(0) <= LFSR4(1);
-                LFSR4(1) <= LFSR4(2);
-                LFSR4(2) <= LFSR4(3);
-                LFSR4(3) <= LFSR4(1) xor LFSR4(0);
-            end if;
+            --generate random for bit3 of PRNG
+            LFSR4(0) <= LFSR4(1);
+            LFSR4(1) <= LFSR4(2);
+            LFSR4(2) <= LFSR4(3);
+            LFSR4(3) <= LFSR4(1) xor LFSR4(0);
         end if;
         
     end process;
