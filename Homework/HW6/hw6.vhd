28d8a41e83837f2dd907cf3485ec2a4073eea13e
Ty <ty.h.ahrens@gmail.com>
2025-03-30 20:20:00 -0500

    Added in given program for accelerometer for DE-10 LITE board

---
+++ Homework/HW6/hw6.vhd
@@ -0,0 +1,16 @@
+--Name: Ty Ahrens 
+--Date: 3/30/2025
+--Purpose: 
+
+library IEEE;
+use IEEE.std_logic_1164.all;
+
+entity hw6 is 
+    port();
+end hw6;
+
+architecture behavior of hw6 is 
+
+
+
+end behavior;
