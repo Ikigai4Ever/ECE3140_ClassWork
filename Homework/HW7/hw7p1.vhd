62c1fb72e0d17729265a32e5f2f0fc5e62b506e9
Ty <ty.h.ahrens@gmail.com>
2025-04-08 10:27:12 -0500

    Add VGA controller implementation for DE10-Lite board

---
+++ Homework/HW7/hw7p1.vhd
@@ -0,0 +1,18 @@
+--      Name: Ty Ahrens
+--      Date: 10/31/2023
+--      Purpose: VGA controller for DE10-Lite board
+
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+entity hw7p1 is 
+    port(SW : IN std_logic_vector(2 downto 0);
+         color_out : std_logic_vector(2 downto 0));
+end hw7p1;
+
+
+
+
+architecture Behavioral of hw7p1 is
+
+end Behavioral;
